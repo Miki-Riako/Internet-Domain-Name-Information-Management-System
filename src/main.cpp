2e7f20a95eb0358c8d9be9495a7f465b71d9b1a6
Miki-Riako <miki_riako@qq.com>
2025-04-24 10:09:19 +0800

    reborn

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-// Internet Domain Name Information Management System
-#include "IDNIMS.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    IDNIMS w;
-    return a.exec();
-}
